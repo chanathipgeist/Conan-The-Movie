60a2d1ec07ad44f8a0175d5ff1e9209dcaa6f094
Chanathip <gzoneaa@gmail.com>
2023-12-09 01:44:40 +0700

    create component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
-
+import NavBar from './components/NavBar.vue'
 </script>
 
 <template>
-  <div class="flex justify-center">Hello</div>
+  <NavBar/>
+  <!-- <div class="flex justify-center">Hello</div> -->
   
 </template>
 
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,13 +1,25 @@
 <script setup>
 //มี Movie Category และ Character
+import { RouterLink } from 'vue-router';
 </script>
  
 <template>
-<div>
-
-</div>
+    <RouterLink :to="{ name: 'MovieList' }">
+        <div>
+            Movie
+        </div>
+    </RouterLink>
+    <RouterLink :to="{ name: 'CategoryView' }">
+        <div>
+            Category
+        </div>
+    </RouterLink>
+    <RouterLink :to="{ name: 'CharacterView' }">
+        <div>
+            Character
+        </div>
+    </RouterLink>
+    
 </template>
  
-<style scoped>
-
-</style>
+<style scoped></style>
